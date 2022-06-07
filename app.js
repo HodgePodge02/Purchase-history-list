1003b4be268e511ce6193369a2a8029bd2f31bee
HodgePodge02 <false4956@gmail.com>
2022-06-06 22:02:02 -0400

    DOM selectors with AddItem function

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
+let Items = [];
 // -- DOM selectors --
 const MainButton = document.getElementById("AddItem");
+const DateInput = document.getElementById("Date");
+const DollarInput = document.getElementById("DollarAmount");
+const CommentInput = document.getElementById("Comment");
+const ListItems = document.getElementById("ListItems");
+// -- Add event listeners --
+MainButton.addEventListener("click", (e) => {
+  const CurrentDateInputValue = DateInput;
+
+  addItem(CurrentDateInputValue.value);
+  console.log(CurrentDateInput);
+});
+
+// -- functions --
+
+function addItem() {
+  Items.unshift({
+    date: DateInput.value,
+    amount: DollarInput.value,
+    comment: CommentInput.value,
+  });
+}
+//putting the elements in an obj
